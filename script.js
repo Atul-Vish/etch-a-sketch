cc5384f9bbb7bbc778bdbd63d59de560eff26030
Atul Vishwakarma <atulvishw240@gmail.com>
2024-05-11 08:24:05 +0530

    Fix a bug: By adding clear() method
    Without clear() method, when user inputs a number our grid got split
    into two; where upper part would be our default(16 x 16) and lower part
    would be the input provided by user (input x input)

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
+let input = 0;
 const container = document.querySelector(".container");
 
-let number = 10;
-for (let i = 0; i < number; i++) {
-    const parentDiv = document.createElement("div");
-    parentDiv.setAttribute("style", "flex: auto; display: flex; height: 60px;");
+setGrid(16);
+// clearGrid();
+changeColor();
 
-    container.appendChild(parentDiv);
-    for (let j = 0; j < number; j++) {
-        const childDiv = document.createElement("div");
-        childDiv.setAttribute("style", "flex: auto; width: 60px;");
-        childDiv.classList.add("red");
+const btn = document.querySelector(".change-grid");
+btn.addEventListener("click", (event) => {
 
-        parentDiv.appendChild(childDiv);
+    input = prompt("Enter number: ", "");
+    if (input > 0 && input <= 100) {
+        clearGrid();
+        setGrid(input);
+    } else {
+        alert("Given value was out of range; So no change in defaults!");
+    }
+
+    changeColor();
+})
+
+function setGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const parentDiv = document.createElement("div");
+        parentDiv.classList.add("sub-container");
+        parentDiv.setAttribute("style", "flex: auto; display: flex; height: 60px; border: 1px solid black;");
+    
+        container.appendChild(parentDiv);
+        for (let j = 0; j < size; j++) {
+            const childDiv = document.createElement("div");
+            childDiv.setAttribute("style", "flex: auto; width: 60px; border: 1px solid black;");
+            childDiv.classList.add("red");
+    
+            parentDiv.appendChild(childDiv);
+        }
     }
 }
 
-const reds = document.querySelectorAll(".red");
-reds.forEach((red) => {
-    red.addEventListener("mouseover", (event) => {
-        red.setAttribute("style", "flex: auto; width: 60px; background-color: red;");
+function changeColor() {
+    const reds = document.querySelectorAll(".red");
+    reds.forEach((red) => {
+        red.addEventListener("mouseover", (event) => {
+            red.setAttribute("style", "flex: auto; width: 60px; background-color: red;");
+        });
     });
-});
+}
+
+// Add a function to remove the old grid
+function clearGrid() {
+    const subContainers = document.querySelectorAll(".sub-container");
+    // const allRedDivs = document.querySelectorAll(".red");
+    // subContainers.forEach((subContainer) => {
+    //     allRedDivs.forEach((redDiv) => {
+    //         subContainer.removeChild(redDiv);
+    //     });
+
+    //     container.removeChild(subContainer);
+    // });
+
+    // for (let i = 0; i < subContainers.length; i++) {
+    //     let item = subContainers[i];
+
+        // const allRedDivs = document.querySelectorAll(".red");
+
+        // while (item.firstChild) {
+        //     item.removeChild(item.firstChild);
+        // }
+
+        // container.removeChild(item);
+
+        // for (let j = 0; j < allRedDivs.length - 1; j++) {
+        //     let redDiv = allRedDivs[j];
+
+        //     item.removeChild(redDiv);
+        // }
+
+        // container.removeChild(item);
+    // }
+
+        subContainers.forEach((item) => {
+            container.removeChild(item);
+        });
+}
