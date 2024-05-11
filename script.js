71892c23b11f6c520d4868878d10c76facfc479a
Atul Vishwakarma <atulvishw240@gmail.com>
2024-05-11 20:04:21 +0530

    Randomize the squares RGB value
    Earlier our squares were getting uniformly colored in red. Now its
    random, if you hover over a div for 'n' times then each time its color
    will be different

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-let input = 0;
+let gridSize = 0;
 const container = document.querySelector(".container");
 
 setGrid(16);
-// clearGrid();
-changeColor();
 
 const btn = document.querySelector(".change-grid");
 btn.addEventListener("click", (event) => {
 
-    input = prompt("Enter num(0-100): ", "");
-    if (input > 0 && input <= 100) {
+    gridSize = prompt("Enter num(0-100): ", "");
+    if (gridSize > 0 && gridSize <= 100) {
         clearGrid();
-        setGrid(input);
+        setGrid(gridSize);
     } else {
         alert("Given value was out of range; So no change in defaults!");
     }
-
-    changeColor();
 })
 
 function setGrid(size) {
     for (let i = 0; i < size; i++) {
         const parentDiv = document.createElement("div");
         parentDiv.classList.add("sub-container");
-        parentDiv.setAttribute("style", "flex: auto; display: flex; height: 60px;");
+        parentDiv.setAttribute("style", "flex: auto; display: flex; border: 0.1px solid whitesmoke;");
     
         container.appendChild(parentDiv);
         for (let j = 0; j < size; j++) {
             const childDiv = document.createElement("div");
-            childDiv.setAttribute("style", "flex: auto; width: 60px;");
+            childDiv.addEventListener('mouseover', setRandomColor);
+            childDiv.setAttribute("style", "flex: auto; border: 0.1px solid whitesmoke;");
             childDiv.classList.add("red");
     
             parentDiv.appendChild(childDiv);
@@ -36,48 +33,20 @@ function setGrid(size) {
     }
 }
 
-function changeColor() {
-    const reds = document.querySelectorAll(".red");
-    reds.forEach((red) => {
-        red.addEventListener("mouseover", (event) => {
-            red.setAttribute("style", "flex: auto; width: 60px; background-color: red;");
-        });
-    });
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + randomColor;
 }
 
-// Add a function to remove the old grid
+function setRandomColor() {
+    const color = getRandomColor();
+    this.style.backgroundColor = color; 
+}
+
 function clearGrid() {
     const subContainers = document.querySelectorAll(".sub-container");
-    // const allRedDivs = document.querySelectorAll(".red");
-    // subContainers.forEach((subContainer) => {
-    //     allRedDivs.forEach((redDiv) => {
-    //         subContainer.removeChild(redDiv);
-    //     });
 
-    //     container.removeChild(subContainer);
-    // });
-
-    // for (let i = 0; i < subContainers.length; i++) {
-    //     let item = subContainers[i];
-
-        // const allRedDivs = document.querySelectorAll(".red");
-
-        // while (item.firstChild) {
-        //     item.removeChild(item.firstChild);
-        // }
-
-        // container.removeChild(item);
-
-        // for (let j = 0; j < allRedDivs.length - 1; j++) {
-        //     let redDiv = allRedDivs[j];
-
-        //     item.removeChild(redDiv);
-        // }
-
-        // container.removeChild(item);
-    // }
-
-        subContainers.forEach((item) => {
-            container.removeChild(item);
-        });
+    subContainers.forEach((item) => {
+        container.removeChild(item);
+    });
 }
